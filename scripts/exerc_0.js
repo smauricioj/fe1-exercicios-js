620b5b32497611c909f725a78d5ec04e3ec94dbe
Sergio Prolo <smauricioj@gmail.com>
2025-05-22 18:22:42 -0300

    Começo

--- scripts/exerc_0.js
+++ scripts/exerc_0.js
@@ -1,7 +1,37 @@
-const PI = 3.1415;
-var nome = "Alice";
-let a = [];
-a[0] = true;
-a[1] = PI;
-a[2] = nome;
-console.log(a);
+function soma(arr) {
+    if(!Array.isArray(arr)) return;
+    let sum = 0;
+    for(var elemento of arr) {
+        sum += Number(elemento);
+    }
+    return sum;
+}
+
+function media(arr) {
+    return soma(arr)/arr.length;
+}
+
+function menor(arr) {
+    let min = Infinity;
+    for(var elemento of arr) {
+        if(elemento < min) {
+            min = elemento;
+        }
+    }
+    return min;
+}
+
+function prata(arr) {
+    let max = [-Infinity, -Infinity];
+    for(var elemento of arr) {
+        if(elemento > max[0]) {
+            max.unshift(elemento);
+            max.pop();
+        }
+    }
+    return max[1];
+}
+
+function filtro(arr) {
+
+}
